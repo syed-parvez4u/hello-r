a395822c2a0470690d778324a6befddd4be8a374
syed-parvez4u <syed.parvez4u@gmail.com>
2021-09-03 22:21:03 +0530

    commit

--- testing.r
+++ testing.r
@@ -58,4 +58,54 @@ unclass(x)
 
 x<-factor(c("yes","yes","no","yes","no"),levels = c("yes","no"))
 x
-          
+
+##Missing values
+x<-c(1,2,NA,10,3)          
+is.na(x)
+is.nan(x)
+x<-c(1,2,NaN,10,3)
+is.nan(x)
+
+##Data Frames
+x<-data.frame(day=1:7,rain=c(T,F,F,T,T,F,T))
+x
+ncol(x)
+nrow(x)
+row.names(x)<-c("A","B","C","D","E","F","G")
+x
+
+
+##Names
+x<-1:3
+names(x)=NULL
+names(x)<-c("Col_A","Col_B")
+x
+
+x<-list(a=1:5,b="ID",c=c("First Name","Last Name"))
+x
+x$a
+x$c
+
+m<-matrix(1:4,nrow=2,ncol=2)
+dimnames(m)<-list(c("r1","r2"),c("col1","col2"))
+m
+
+##Subset
+x<-c("a","b","c","c","d","a")
+x[1]
+x[2]
+x[1:4]
+x[x>"a"]
+
+u<-x>"a"
+u
+
+##Subsetting a matrix
+x<-matrix(1:6,2,3)
+x
+x[1,2]
+
+x[1,2,drop=FALSE]
+
+
+
